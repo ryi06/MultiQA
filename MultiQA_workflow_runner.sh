d25dc94ace1fe22da59d904fd4238ca994400aab
Ren Yi <ryi06@hotmail.com>
2018-04-08 18:30:26 -0400

    update runnder file

--- MultiQA_workflow_runner.sh
+++ MultiQA_workflow_runner.sh
@@ -15,39 +15,49 @@
 
 
 ################## Configs ##################
-dataset="SQuAD"
+dataset="QUASAR-T"
+data_dir="data/custom_datasets"
+length="long"
 num_paragraphs=$SLURM_ARRAY_TASK_ID
 model_dir="saved_model/${dataset}"
 today=$(date +%Y%m%d)
 model_name="${num_paragraphs}_${dataset}_${today}"
+declare -a data_types=("train" "dev")
 
 cd /scratch/ry708/CSCI/dsga_1012/project/MultiQA
 [[ ! -d ${model_dir} ]] && mkdir -p ${model_dir}
 
-
 # Generate dataset
-# Result file saved in form
+for dt in "${data_types[@]}"
+do
+	context_file="${data_dir}/${dataset}_${dt}_${length}_contexts.json"
+	question_file="${data_dir}/${dataset}_${dt}_questions.json"
+	java -jar scripts/preprocessing/quasar_data_prep.jar \
+	${context_file} \
+	${question_file} \
+	${SLURM_JOBTMP} \
+	${num_paragraphs}
 
+	prefix="${num_paragraphs}_${dataset}_${length}_{dt}"
+	python scripts/reader/preprocess.py \
+	${SLURM_JOBTMP} \
+	${SLURM_JOBTMP} \
+	--split p${prefix}
+done
+
+# Data generated from previous steps
+train_data="${num_paragraphs}_${dataset}_${length}_train-processed-corenlp.txt"
+dev_data="${num_paragraphs}_${dataset}_${length}_dev-processed-corenlp.txt"
 # Train on HPC
 python scripts/reader/train.py \
 --embedding-file glove.840B.300d.txt \
---data-dir data/custom_datasets \
+--data-dir ${SLURM_JOBTMP} \
 --model-dir ${model_dir} \
 --model-name ${model_name} \
+--train-file ${train_data} \
+--dev-file ${dev_data} \
 --tune-partial 1000 \
---num-epochs 5
-# --train-file
-# --dev-file
-
-
-
-# Train local
-# python scripts/reader/train.py \
-# --embedding-file glove.840B.300d.txt \
-# --data-dir data/custom_datasets \
-# --no-cuda True \
-# --tune-partial 1000 \
-# --checkpoint True \
-# --num-epochs 1
-
+--train-file ${train_data} \
+--dev-file ${dev_data} \
+--num-epochs 1
 
